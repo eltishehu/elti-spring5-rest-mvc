f78a41a2b938cb052b5b75e01eb7a2bc049dc331
Elti Shehu <shehuelti@gmail.com>
2018-10-18 16:51:59 +0200

    Added feature to Update Customer with PATCH

--- src/main/java/com/eltishehu/restmvcproject/services/CustomerServiceImpl.java
+++ src/main/java/com/eltishehu/restmvcproject/services/CustomerServiceImpl.java
@@ -98,7 +98,11 @@ public class CustomerServiceImpl implements CustomerService {
                         customer.setLastname(customerDTO.getLastname());
                     }
 
-                    return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+                    CustomerDTO returnDTO = customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+                    returnDTO.setCustomerUrl("/api/v1/customer/" + id);
+
+                    return returnDTO;
+
                 }).orElseThrow(RuntimeException::new); //TODO implement better exception handling
 
     }

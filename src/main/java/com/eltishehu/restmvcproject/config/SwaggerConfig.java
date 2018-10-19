59a6c76c00bbfb7e61e1c03e056485c3c96039cc
Elti Shehu <shehuelti@gmail.com>
2018-10-19 12:43:04 +0200

    Added swagger dependency and basic configuration

---
+++ src/main/java/com/eltishehu/restmvcproject/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.eltishehu.restmvcproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by e.sh. on 19-Oct-18
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig { // extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+
+    }
+
+    //if you're not running spring boot and dont see swagger ui, you need to add these configurations
+    /*@Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
+}
